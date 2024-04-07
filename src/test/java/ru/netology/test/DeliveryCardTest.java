4d0dcac83639431e07792573a9d037a127a959e4
Tatiana Titova <ritasom666@gmail.com>
2024-04-07 19:11:04 +0400

    ex.1 start

--- src/test/java/ru/netology/test/DeliveryCardTest.java
+++ src/test/java/ru/netology/test/DeliveryCardTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -15,13 +17,17 @@ public class DeliveryCardTest {
 @Test
     void shouldSendSuccess(){
     open("http://localhost:9999/");
+
+
     $("[data-test-id='city'] .input__control").sendKeys("Калининград");
     SelenideElement element = $("[data-test-id='date'] .input__control");
     String value = $("[data-test-id='date'] .input__control").getValue();
     for (int i = 0; i < value.length(); i++) {
         element.sendKeys(Keys.BACK_SPACE);
+
     }
-    $("[data-test-id='date'] .input__control").sendKeys("11.04.2024");
+    String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    $("[data-test-id='date'] .input__control").sendKeys(date);
     $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
     $("[data-test-id='phone'] .input__control").sendKeys("+79850001122");
     $("[data-test-id='agreement']").click();
