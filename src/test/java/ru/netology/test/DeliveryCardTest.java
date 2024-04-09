8474d7f949b14446f896232b28c00a7b8caa9cda
Tatiana Titova <ritasom666@gmail.com>
2024-04-09 20:40:03 +0400

    start ex.2

--- src/test/java/ru/netology/test/DeliveryCardTest.java
+++ src/test/java/ru/netology/test/DeliveryCardTest.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -310,5 +311,28 @@ public class DeliveryCardTest {
         $(".button").click();
         $("label.checkbox.input_invalid").shouldBe(Condition.exist);
 
+    }
+
+    @Test
+    void shouldSendSuccessAndCityMenu() {
+        open("http://localhost:9999/");
+
+
+        $("[data-test-id='city'] .input__control").sendKeys("Ек");
+        $(By.cssSelector(".menu_mode_radio-check > div:nth-child(4)")).hover().click();
+
+        SelenideElement element = $("[data-test-id='date'] .input__control");
+        String value = $("[data-test-id='date'] .input__control").getValue();
+        for (int i = 0; i < value.length(); i++) {
+            element.sendKeys(Keys.BACK_SPACE);
+
+        }
+        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='date'] .input__control").sendKeys(date);
+        $("[data-test-id='name'] .input__control").sendKeys("Иван Иванов");
+        $("[data-test-id='phone'] .input__control").sendKeys("+79850001122");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 }
